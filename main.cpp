221993147bae2bf65212d2d4f4b7a53eff41b3e8
mitchmcdee <mitchmcdee@gmail.com>
2016-11-14 22:34:25 +1000

    Added DFS search for connected cells upon a move-- will currently turn the connected cells black if more than 3 cells of the same colour are connected

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
-int* initBoard(int* board, int width, int height) {
+int* initBoard(int*, int, int);
+void swapTiles(int*, int, int, int, int, int, int);
+void checkConnections(int* board, int width, int height, int stack[][2], int visited[][2], int *stackCount, int *visitedCount, int sourceX, int sourceY);
+
+
+    int* initBoard(int* board, int width, int height) {
     for(int x = 0; x < width; x++) {
         for(int y = 0; y < height; y++) {
             board[x*width + y] = rand() % 5;
@@ -10,7 +15,8 @@ int* initBoard(int* board, int width, int height) {
     return board;
 }
 
-void swapTiles(int* board, int width, int x1, int y1, int x2, int y2) {
+
+void swapTiles(int* board, int width, int height, int x1, int y1, int x2, int y2) {
     int xDiff = abs(x1 - x2);
     int yDiff = abs(y1 - y2);
 
@@ -22,14 +28,57 @@ void swapTiles(int* board, int width, int x1, int y1, int x2, int y2) {
     int tempTile = board[x1*width + y1];
     board[x1*width + y1] = board[x2*width + y2];
     board[x2*width + y2] = tempTile;
+
+    int visited[width*height][2];
+    int stack[width*height][2];
+    int visitedCount = 0;
+    int stackCount = 0;
+    checkConnections(board, width, height, stack, visited, &stackCount, &visitedCount, x2, y2);
+    if (visitedCount >= 3) {
+        for (int i = 0; i < visitedCount; i++) {
+            board[visited[i][0] * width + visited[i][1]] = -1;
+        }
+    }
 }
 
+
+//DFS with source as the final swap pos
+void checkConnections(int* board, int width, int height, int stack[][2], int visited[][2], int *stackCount, int *visitedCount, int sourceX, int sourceY) {
+    for(int i = 0; i < *stackCount; i++) {
+        if (stack[i][0] == sourceX && stack[i][1] == sourceY) {
+            return;
+        }
+    }
+
+    stack[*stackCount][0] = sourceX;
+    stack[(*stackCount)++][1] = sourceY;
+
+    // ULDR heirachy DFS
+    if (sourceY-1 >= 0 && board[sourceX*width + sourceY-1] == board[sourceX*width + sourceY]) {
+        checkConnections(board, width, height, stack, visited, stackCount, visitedCount, sourceX, sourceY-1);
+    }
+    if (sourceX-1 >= 0 && board[(sourceX-1)*width + sourceY] == board[sourceX*width + sourceY]) {
+        checkConnections(board, width, height, stack, visited, stackCount, visitedCount, sourceX-1, sourceY);
+    }
+    if (sourceY+1 < height && board[sourceX*width + sourceY+1] == board[sourceX*width + sourceY]) {
+        checkConnections(board, width, height, stack, visited, stackCount, visitedCount, sourceX, sourceY+1);
+    }
+    if (sourceX+1 < width && board[(sourceX+1)*width + sourceY] == board[sourceX*width + sourceY]) {
+        checkConnections(board, width, height, stack, visited, stackCount, visitedCount, sourceX+1, sourceY);
+    }
+    visited[*visitedCount][0] = sourceX;
+    visited[(*visitedCount)++][1] = sourceY;
+}
+
+
+
 int main() {
-    int windowWidth = 1000;
-    int windowHeight = 1000;
+    unsigned int windowWidth = 1000;
+    unsigned int windowHeight = 1000;
     int boardWidth = 10;
     int boardHeight = 10;
     int gap = 10;
+
     int board[boardWidth * boardHeight];
     int prevSelectedX = -1;
     int prevSelectedY = -1;
@@ -65,11 +114,6 @@ int main() {
 
                 case sf::Event::MouseButtonPressed:
                     if (event.mouseButton.button == sf::Mouse::Left) {
-                        std::cout << "the left button was pressed" << std::endl;
-                        std::cout << "mouse x: " << event.mouseButton.x << std::endl;
-                        std::cout << "mouse y: " << event.mouseButton.y << std::endl;
-                        std::cout << "tile x: " << event.mouseButton.x / (windowWidth/boardWidth) << std::endl;
-                        std::cout << "tile y: " << event.mouseButton.y / (windowHeight/boardHeight) << std::endl;
 
                         int selectedX = event.mouseButton.x / (windowWidth/boardWidth);
                         int selectedY = event.mouseButton.y / (windowHeight/boardHeight);
@@ -77,7 +121,8 @@ int main() {
                             prevSelectedX = selectedX;
                             prevSelectedY = selectedY;
                         } else {
-                            swapTiles(board, boardWidth, selectedX, selectedY, prevSelectedX, prevSelectedY);
+                            swapTiles(board, boardWidth, boardHeight,
+                                      selectedX, selectedY, prevSelectedX, prevSelectedY);
                             prevSelectedX = -1;
                             prevSelectedY = -1;
                         }
@@ -89,16 +134,14 @@ int main() {
             }
         }
 
-        // clear the window with black color
         window.clear(sf::Color::Black);
 
         for(int x = 0; x < boardWidth; x++) {
             for(int y = 0; y < boardHeight; y++) {
-                sf::RectangleShape rectangle(sf::Vector2f(cellWidth, cellHeight));
 
-                int tile = board[x*boardWidth + y];
+                sf::RectangleShape rectangle(sf::Vector2f(cellWidth, cellHeight));
                 sf::Color color;
-                switch(tile) {
+                switch(board[x*boardWidth + y]) {
                     case 0:
                         color = sf::Color::White;
                         break;
@@ -115,20 +158,11 @@ int main() {
                         color = sf::Color::Yellow;
                         break;
                     default:
-                        std::cout << "Whoa that's wrong" << std::endl;
+                        color = sf::Color::Black;
                         break;
                 }
 
-                if (x == prevSelectedX && y == prevSelectedY) {
-                    rectangle.setOutlineThickness(gap/2);
-                    if (tile == 0) {
-                        rectangle.setOutlineColor(sf::Color::Red);
-                    } else {
-                        rectangle.setOutlineColor(sf::Color::White);
-                    }
-                }
-
-                if (x == mouseX && y == mouseY) {
+                if ((x == prevSelectedX && y == prevSelectedY) || (x == mouseX && y == mouseY)) {
                     color.a = 128;
                 }
 
